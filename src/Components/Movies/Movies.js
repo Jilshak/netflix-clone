d7b485d9aada5ead8e2163179bae79c20bb959e3
Jilshak <mohd.jilshak@gmail.com>
2023-01-13 23:56:32 +0530

    added the youtube react functionality to the movies section

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -2,9 +2,24 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from '../../Constants/Axios'
 import '../Movies/Movies.css'
+import YouTube from 'react-youtube'
 import { API_KEY, baseIMG } from '../../Constants/Constants'
 
 function Movies(props) {
+
+
+    const [moviesid, setMoviesId] = useState('')
+    // function to get the video id from the API so that we can use it to view the youtube video in react-youtube
+    const handleClick = (id) => {
+        axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response) => {
+            if (response.data.results.length !== 0) {
+                setMoviesId(response.data.results[0])
+            };
+
+        })
+    }
+
+
 
     const [movies, setMovie] = useState([])
 
@@ -17,8 +32,25 @@ function Movies(props) {
         })
     }, [])
 
+    //when the video ended this will remove the youtube video player
+    const endVideo = () => {
+        setMoviesId('')
+    }
 
 
+    //these are the function of the video player
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+
+    };
+
+
+    
     // function to map the moveis and render it accordingly on the screen
     return (
 
@@ -30,15 +62,16 @@ function Movies(props) {
                         if (item.backdrop_path) {
                             return (
                                 <div>
-                                    <img className='items' src={`${baseIMG}${item.backdrop_path}`} alt="" />
+                                    <img onClick={() => handleClick(item.id)} className='items' src={`${baseIMG}${item.backdrop_path}`} alt="" />
                                     <h5 className='titles'>{item.title ? item.title : item.name}</h5>
                                 </div>
                             )
-                        }else{
+                        } else {
                             return ''
                         }
                     })}
                 </div>
+                {moviesid && <YouTube opts={opts} videoId={moviesid.key} onEnd={endVideo} />}
             </h3>
         </div>
     )
